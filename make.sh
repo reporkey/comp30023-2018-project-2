ce2edaca5b941b1f2efcfdd714c7e118afd4e8ec
Zichun Zhu <z.zhu11@student.unimelb.edu.au>
2018-05-21 14:28:20 +1000

    readme

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-fullname="certcheck.c"
-option="-lssl -lcrypto"
-name=`echo $fullname | cut -d. -f1`
-
-gcc -o $name $fullname $option
-echo ========================
-./$name ./sample_input.csv
